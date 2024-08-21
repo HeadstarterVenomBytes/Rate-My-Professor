16ea23ae2b5daa4f64c685c00a497ae0dcfcf86f
peppapig450 <nbgenius1@gmail.com>
2024-08-21 12:39:04 -0700

    write types that will be used when scraping individual professor's pages

---
+++ src/types/professorDataTypes.ts
@@ -0,0 +1,52 @@
+enum YesNo {
+  Yes = "Yes",
+  No = "No",
+}
+
+enum Attendance {
+  Mandatory = "Mandatory",
+  NotMandatory = "Not Mandatory",
+}
+
+enum Grade {
+  A = "A",
+  B = "B",
+  C = "C",
+  D = "D",
+  F = "F",
+  NotSureYet = "Not sure yet",
+}
+
+export interface Professor {
+  name: string;
+  department: string;
+  university: string;
+  averageRating: number;
+  numRatings: number;
+  reviews: ProfessorReview[];
+  ratingDistribution: {
+    fiveStar: number;
+    fourStar: number;
+    threeStar: number;
+    twoStar: number;
+    oneStar: number;
+  };
+  tags: string[];
+}
+
+export interface ProfessorReview {
+  quality: number;
+  difficulty: number;
+  course: string;
+  date: Date;
+  review: string;
+  helpfulVotes: number;
+  unhelpfulVotes: number;
+  textbook?: YesNo;
+  forCredit?: YesNo;
+  attendence?: Attendance;
+  grade?: Grade;
+  wouldTakeAgain?: YesNo;
+  tags?: string[];
+  professor: Professor;
+}
