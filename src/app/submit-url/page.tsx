c5a583d87445e50f7ddcf814039a456c8f9aafca
peppapig450 <nbgenius1@gmail.com>
2024-08-21 18:00:11 -0700

    page for that form with paper behind it

---
+++ src/app/submit-url/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container, Paper } from "@mui/material";
+import UrlSubmissionForm from "../components/UrlSubmissionForm";
+
+// TODO: write hook for the api request
+export default function SubmitUrlPage(): React.JSX.Element {
+  const handleSubmitUrl = (url: string) => {
+    // Call hook for sending the url to the backend for scraping
+    console.log("URL submitted for scraping:", url);
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Paper elevation={3} sx={{ p: 4, mt: 4 }}>
+        <UrlSubmissionForm onSubmit={handleSubmitUrl} />
+      </Paper>
+    </Container>
+  );
+}
