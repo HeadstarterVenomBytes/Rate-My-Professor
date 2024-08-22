513afb2222e19dfdac78283584abe1cab6396247
peppapig450 <nbgenius1@gmail.com>
2024-08-21 23:32:17 -0700

    write api route for handling the scraping

--- src/app/api/scrape/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { scrapeProfessorPage } from "@/lib/utils/rateMyProfScraper";
-
-export async function POST(req: NextRequest): Promise<NextResponse> {
-    try {
-        const url = req.body;
-    }
-}
