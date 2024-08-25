e8aba0a8c301c651175634513e283e17629c061b
peppapig450 <nbgenius1@gmail.com>
2024-08-25 16:17:00 -0700

    get rid of chat request as it hasn't been a chat bot for a while

--- src/types/review.ts
+++ src/types/review.ts
@@ -1,8 +1,3 @@
-export interface ChatRequest {
-  role: "user" | "assistant";
-  content: string;
-}
-
 // Define the interface for the expected output
 export interface ProfessorRecommendation {
   name: string;
