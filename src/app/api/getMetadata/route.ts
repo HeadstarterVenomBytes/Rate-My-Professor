b9f8323f6613064c63e109953be24ae91266eb91
peppapig450 <nbgenius1@gmail.com>
2024-08-25 16:21:04 -0700

    type the NextResponse with the metadata type

--- src/app/api/getMetadata/route.ts
+++ src/app/api/getMetadata/route.ts
@@ -1,16 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import { fetchMetadataSets } from "@/lib/utils/fetchPineconeMetadata";
+import { MetadataResult } from "@/types/pineconeMetadata";
 
-export default async function GET(req: NextRequest): Promise<NextResponse> {
+export default async function GET(
+  req: NextRequest
+): Promise<NextResponse<MetadataResult | { error: string }>> {
   try {
     const indexName = process.env.PINECONE_INDEX_NAME_2!;
 
     const metadata = await fetchMetadataSets(indexName);
 
-    return NextResponse.json({
-      universities: metadata.university,
-      departments: metadata.department,
-    });
+    return NextResponse.json(metadata);
   } catch (error) {
     console.error("Error retrieving pinecone metadata sets:", error);
     return NextResponse.json(
