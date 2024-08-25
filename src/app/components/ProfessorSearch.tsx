fe1388242eb4ebf35c314f641707e46bf922f76e
peppapig450 <nbgenius1@gmail.com>
2024-08-25 11:51:57 -0700

    update the display page to display all the new information

--- src/app/components/ProfessorSearch.tsx
+++ src/app/components/ProfessorSearch.tsx
@@ -67,12 +67,32 @@ const ProfessorSearch: React.FC<ProfessorSearchProps> = ({}) => {
             <Grid item xs={12} sm={6} md={4} key={index}>
               <Paper elevation={3} sx={{ p: 2 }}>
                 <Typography variant="h6">{prof.name}</Typography>
-                <Typography variant="body2">Subject: {prof.subject}</Typography>
+                <Typography variant="body2">
+                  Department: {prof.department}
+                </Typography>
+                <Typography variant="body2">
+                  University: {prof.university}
+                </Typography>
                 {/* display highlighted stars based on number? */}
                 <Typography variant="body2">
-                  Rating: {prof.rating} stars
+                  Average Rating: {prof.averageRating.toFixed(2)} stars
                 </Typography>
-                <Typography variant="body2">{prof.review}</Typography>
+                <Typography variant="body2">
+                  Top Reviews Avg: {prof.topReviewsAvgRating.toFixed(2)} stars
+                </Typography>
+                <Typography variant="body2">
+                  Number of Ratings: {prof.numRatings}
+                </Typography>
+                <Typography variant="body2">
+                  Would Take Again: {prof.wouldTakeAgainPercentage.toFixed(2)}%
+                </Typography>
+                <Typography variant="body2">Tags: {prof.tags}</Typography>
+                <Typography variant="body2">
+                  Reviews: {prof.reviews_summary}
+                </Typography>
+                <Typography variant="body2" sx={{ mt: 1, fontStyle: "italic" }}>
+                  Explanation: {prof.explanation}
+                </Typography>
               </Paper>
             </Grid>
           ))
